e933e71682985afb1853eed4a0c95ee3391db03e
SharefRRC <sislam48@academic.rrc.ca>
2026-06-19 16:51:47 -0500

    feat add public health check route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import healthRoutes from "./api/v1/routes/health.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api/v1", healthRoutes);
 
 export default app;
